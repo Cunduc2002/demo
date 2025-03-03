8e280d7c77139b7bb5500199e07f516c34afd986
Cunduc2002 <hanhducquannguyen@gmail.com>
2025-03-03 10:21:15 -0600

    added entities (User.java) repositories (UserRepository.java) UserController.java in controllers

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controllers;
+
+import com.example.demo.service.UserService;
+import entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+}
